588b7ed3e8a6a1a500cc588ed50f697569a36a05
ScRien <sah.fdn25@gmail.com>
2025-11-21 19:46:51 +0300

    legal policies fix

--- routes/legal.js
+++ routes/legal.js
@@ -2,12 +2,12 @@ import express from "express";
 
 const legalRouter = express.Router();
 
-legalRouter.get("/kvkk", (req, res) => {
-  res.render("pages/legal/kvkk");
+legalRouter.get("/acik-riza-metni", (req, res) => {
+  res.render("pages/legal/acik-riza-metni");
 });
 
-legalRouter.get("/gizlilik", (req, res) => {
-  res.render("pages/legal/gizlilik");
+legalRouter.get("/gizlilik-politikasi", (req, res) => {
+  res.render("pages/legal/gizlilik-politikasi");
 });
 
 legalRouter.get("/kullanim-sartlari", (req, res) => {
